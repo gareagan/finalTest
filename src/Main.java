7f59d4ae2abb36c8c5cc5c9ad1619825b84371e7
gareagan <gareagan@buffalo.edu>
2023-11-17 12:50:32 -0500

    commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int store = helperMain(2,5);
+        System.out.println(store);
+
+    }
+
+    public static int helperMain(int add1, int add2)
+    {
+        return add1 + add2;
+    }
+}
